18a9f93345260155ea7aefef3ab4042225a5b81b
KevinDavoust <kevin.davoust@orange.fr>
2024-03-26 16:09:20 +0100

    3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,5 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const PokemonCard = () => {
-  const pokemon = pokemonList[0];
-
+import PropTypes from "prop-types";
+const PokemonCard = ({ pokemon }) => {
   return (
     <>
       <figure>
@@ -25,6 +13,13 @@ const PokemonCard = () => {
       </figure>
     </>
   );
+};
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    imgSrc: PropTypes.string,
+    name: PropTypes.string.isRequired,
+  }),
 };
 
 export default PokemonCard;
